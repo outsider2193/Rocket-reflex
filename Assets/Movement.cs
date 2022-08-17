9a690477c54cd8071112899f2e30dc5b1662bc4e
Kushalsingh Chauhan <c2kushalsingh@gmail.com>
2022-08-17 18:04:24 +0530

    dumping the work done so far

---
+++ Assets/Movement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  Rigidbody rbody;
+  [SerializeField] float Thrust=0f;
+  [SerializeField] float RotationThrust=0f;
+    void Start()
+    {
+      
+        rbody= GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrusting();
+    }
+
+    void Thrusting(){
+        
+            if(Input.GetKey(KeyCode.Space))
+            {
+              rbody.AddRelativeForce(Vector3.up*Thrust*Time.deltaTime);
+            }
+          
+             
+            
+        
+
+          {
+                  Rotating();
+          }   
+                 void Rotating(){
+                  rbody.freezeRotation=true;
+                  rbody.freezeRotation=false;
+                 {}
+        if(Input.GetKey(KeyCode.A)){
+        transform.Rotate(Vector3.back*RotationThrust*Time.deltaTime);
+        }
+       
+        else if(Input.GetKey(KeyCode.D)){
+         transform.Rotate(Vector3.forward*RotationThrust*Time.deltaTime);
+       }
+    
+
+
+       
+    
+
+        
+     
+
+            }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        } 
+}
+
